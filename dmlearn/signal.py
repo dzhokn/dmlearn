00dfc861071143fe9f0207678cca589b395480fa
Miroslav Dzhokanov <mdzhokanov@gmail.com>
2025-09-26 16:55:20 +0300

    Update docstring

--- dmlearn/signal.py
+++ dmlearn/signal.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 def pad_tensor(
-        tensor: np.ndarray, 
-        padding: int, 
+        tensor: np.ndarray,
+        padding: int,
         fill_value: float = 0.0) -> np.ndarray:
     """
-    Pad a 2D or 3D tensor with a given value. 
+    Pad a 2D or 3D tensor with a given value.
     e.g. [[1,2], [3,4]] -> [[0, 0, 0, 0], [0, 1, 2, 0], [0, 3, 4, 0], [0, 0, 0, 0]]
 
     Args:
         tensor: The tensor to pad (e.g. [[1,2], [3,4]])
-        padding_offset: The number of elements to pad on each side (e.g. 1)
-        padding_fill_value: The value to pad the tensor with (default value is 0)
+        padding: The number of elements to pad on each side (e.g. 1)
+        fill_value: The value to pad the tensor with (default value is 0)
 
     Returns:
         The padded tensor (e.g. [[0, 0, 0, 0], [0, 1, 2, 0], [0, 3, 4, 0], [0, 0, 0, 0]])
